8d47dc1449d2253fb2b14ae68a72f36f4debecf2
shuang <570340350@qq.com>
2019-01-16 16:20:13 +0800

    day1 res

---
+++ day1_html与javascript与css/html/cc.js
@@ -0,0 +1,11 @@
+
+   function cc() {
+       for (let i = 0; i <10; i++) {
+           setTimeout(function() {
+               console.log(i);
+           }, 0);
+       }
+
+   }
+
+   var jije= {}
